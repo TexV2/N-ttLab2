bf0d8559b468ee7d6b234ce7ea95bb85a6ccf9f5
Tex <carl.everman@gmail.com>
2026-05-15 18:13:07 +0200

    TCP

--- receiver.py
+++ receiver.py
@@ -1,17 +1,36 @@
 import socket
+# Empty string means listen on all network interfaces
 HOST = ""
 PORT = 5005
 expected = 10001
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#Creates UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Binds socket to PORT
 sock.bind((HOST, PORT))
+
+#TCP listening for an incoming connection
+sock.listen(1)
 print("Listening...")
 
+#Accept incoming connection
+conn, addr =sock.accept()
+print(f"Connected to {addr}")
+
+#Constant loop 
 while True:
-    data, addr = sock.recvfrom(2048)
+    #Waiting for the next packet, 2048 is the max bytes
+    data = conn.recv(2048)
+    if not data:
+        break
+    #Decodes bytes into string
     msg = data.decode()
+    #Removes the unecessary parts of the string
     seq_str = msg.split(";")[0]
+    #Converts string to int
     seq = int(seq_str)
+    #Checks if the packet arrived in the correct order
     if seq != expected:
         print(f"ORDER ERROR: expected {expected}, got {seq}")
     else:
--- sender.py
+++ sender.py
@@ -1,19 +1,33 @@
 import socket, time
 
-HOST = "RECEIVER_IP"
+#Sets the IP addres 192.168.0.33 (my laptop currently) as HOST
+HOST = "192.168.0.33"
+#Sets the PORT to 5005
 PORT = 5005
-RATE = 1
-NUMBER_OF_PACKETS = 60
+#Sets the rate at which packets will be sent
+RATE = 46
+#Sets how many packets will be sent in total
+NUMBER_OF_PACKETS = 600
+#Selects what the starting message on the packet will be
 SEQUENCE_START = 10001
 
+#Creates a UDP socket using IPv4
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#Sets the delay
 delay = 1.0 / RATE
 
+#Sends on packet for each i in range number of packets
 for i in range(NUMBER_OF_PACKETS):
+    #Creates the order verification number that will be read by the receiver
     seq = str(SEQUENCE_START + i) + ";"
+    #Creates the message filler to fulfill the 1465 size requirment
     payload = ("A" * 1465)[:1465]
+    #Combines the order verification number with the "junk" data
     msg = (seq + payload + "####").encode()
+    #Sends the message to HOST's (laptop) PORT 5005
     sock.sendto(msg, (HOST, PORT))
+    #Pauses the script for one second
     time.sleep(delay)
 
+#Close the socket
 sock.close()
